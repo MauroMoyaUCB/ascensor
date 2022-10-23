1b83ab7489d40344053901b5d6a7b9233783337c
Mauro Moya <99930598+MauroMoyaUCB@users.noreply.github.com>
2022-10-22 23:19:26 -0400

    add: PersonaTest -Fix: Mejora de test elevador

--- src/test/java/com/ucb/tdd/AscensorTest.java
+++ src/test/java/com/ucb/tdd/AscensorTest.java
@@ -5,309 +5,53 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-
-
-import static org.junit.Assert.assertEquals;
-
-
-
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import static org.junit.Assert.*;
 
 public class AscensorTest {
 
-
-
-        @Rule
-
-        public ExpectedException exception= ExpectedException.none();
-
-
-
-        private ascensor ascensor;
-
-
-
-        @Before
-
-        public void before() {
-
-            ascensor =  new ascensor();
-
-        }
-
-
-
-        @Test
-
-        public void cuandoFueraDeXlanzarException() throws Exception{
-
-//1. Preparacion de la prueba
-
-
-
-            System.out.println("==================================== "+ascensor);
-
-
-
-//2. Logica de la prueba
-
-
-
-//3. Verificacion o Assert
-
-            exception.expect(Exception.class);
-
-            ascensor.jugar(5,2);
-
-        }
-
-
-
-        @Test
-
-        public void cuandoFueraDeYlanzarException() throws Exception{
-
-//1. Preparacion de la prueba
-
-
-
-            System.out.println("==================================== "+ascensor);
-
-
-
-//2. Logica de la prueba
-
-
-
-//3. Verificacion o Assert
-
-            exception.expect(Exception.class);
-
-            ascensor.jugar(2,5);
-
-        }
-
-
-
-        @Test
-
-        public void cuandoLugarOcupadoLanzarException() throws Exception{
-
-//1. Preparacion de la prueba
-
-
-
-            System.out.println("==================================== "+ascensor);
-
-
-
-//2. Logica de la prueba
-
-            exception.expect(Exception.class);
-
-            ascensor.jugar(1,3); //Jugador X
-
-
-
-//3. Verificacion o Assert
-
-            exception.expect(Exception.class);
-
-            ascensor.jugar(1,3);//Jugador +
-
-        }
-
-
-
-        @Test
-
-        public void siPrimerTurnoEntoncesJuegaX() throws Exception{
-
-//1. Preparacion de la prueba
-
-
-
-            System.out.println("==================================== "+ascensor);
-
-
-
-//2. Logica de la prueba
-
-
-
-//3. Verificacion o Assert
-
-            assertEquals('X',ascensor.siguienteTurno());//Jugador X
-
-        }
-
-
-
-        @Test
-
-        public void siPrimerTurnoXsiguienteCruz() throws Exception{
-
-//1. Preparacion de la prueba
-
-
-
-            System.out.println("==================================== "+ascensor);
-
-
-
-//2. Logica de la prueba
-
-            ascensor.jugar(1,3); //Jugador X
-
-
-
-//3. Verificacion o Assert
-
-            assertEquals('+',ascensor.siguienteTurno());//Jugador +
-
-        }
-
-
-
-        @Test
-
-        public void siUltimoTurnoCruzSiguienteX() throws Exception{
-
-//1. Preparacion de la prueba
-
-
-
-            System.out.println("==================================== "+ascensor);
-
-
-
-//2. Logica de la prueba
-
-            ascensor.jugar(1,3); //Jugador X
-
-
-
-//3. Verificacion o Assert
-
-            assertEquals('+',ascensor.siguienteTurno());
-
-
-
-            ascensor.jugar(2,3); //Jugador +
-
-            assertEquals('X',ascensor.siguienteTurno());
-
-        }
-
-
-
-        @Test
-
-        public void cuandoNoHayGanador() throws Exception{
-
-//1. Preparacion de la prueba
-
-
-
-            System.out.println("==================================== "+ascensor);
-
-
-
-//2. Logica de la prueba
-
-            String resultado = ascensor.jugar(2,1); //Jugador X
-
-
-
-//3. Verificacion o Assert
-
-            assertEquals("No hay ganador aun",resultado);
-
-        }
-
-
-
-        @Test
-
-        public void siLlenaFilaEntoncesGana() throws Exception{
-
-
-
-
-
-//1. Preparacion de la prueba
-
-
-
-            System.out.println("==================================== "+ascensor);
-
-
-
-//2. Logica de la prueba
-
-            ascensor.jugar(1,1); //Jugador X
-
-            ascensor.jugar(1,2); //Jugador +
-
-            ascensor.jugar(2,1); //Jugador X
-
-            ascensor.jugar(2,2); //Jugador +
-
-
-
-            String resultado = ascensor.jugar(3,1); //Jugador X
-
-
-
-//3. Verificacion o Assert
-
-            assertEquals("X es el ganador",resultado);
-
-        }
-
-
-
-        @Test
-
-        public void siLlenaColumnaEntoncesGana() throws Exception{
-
-
-
-
-
-//1. Preparacion de la prueba
-
-
-
-            System.out.println("==================================== "+ascensor);
-
-
-
-//2. Logica de la prueba
-
-            ascensor.jugar(2,1); //Jugador X
-
-            ascensor.jugar(1,1); //Jugador +
-
-            ascensor.jugar(3,1); //Jugador X
-
-            ascensor.jugar(1,2); //Jugador +
-
-            ascensor.jugar(2,2); //Jugador X
-
-
-
-            String resultado = ascensor.jugar(1,3); //Jugador +
-
-
-
-//3. Verificacion o Assert
-
-            assertEquals("+ es el ganador",resultado);
-
-        }
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    private Elevador elevador;
+
+    @Before
+    public void before() {
+        elevador = new Elevador();
+    }
+
+    @Test
+    public void solounapersonaenelelevador() throws Exception {
+        elevador.personaSubiendoElevador();
+        exception.expect(Exception.class);
+        elevador.personaSubiendoElevador();
+    }
+
+    @Test
+    public void primetoarribaluegoabajo() {
+        assertEquals(3, elevador.movimientoDeElevador(1, 3));
+        assertEquals(1, elevador.movimientoDeElevador(3, 1));
+        assertEquals(3, elevador.movimientoDeElevador(2, 3));
+        assertEquals(1, elevador.movimientoDeElevador(2, 1));
 
     }
 
+    @Test
+    public void solotrespersonasenelelevador() {
+        int floors = 3;
+        assertEquals(floors, elevador.getPisos());
+    }
 
+    @Test
+    public void aliniciarelelevadorenprimerpiso() {
+        assertFalse(elevador.getElevadorEstado());
+    }
+
+    @Test
+    public void personaDentroElevador() throws Exception {
+        elevador.personaEnElevador(1);
+    }
+}
